141d4437eeeea44160e704a7554ec5800da9ff6e
Guy Hershenbaum <hershi@fb.com>
2018-12-02 20:30:04 -0800

    AoC 2018 - day 2 complete

--- aoc_2/src/main.rs
+++ aoc_2/src/main.rs
@@ -1,3 +1,45 @@
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+use std::collections::HashMap;
+
+fn read_input() -> Vec<String> {
+    let input_file = File::open("src/input.txt").unwrap();
+    let reader = BufReader::new(input_file);
+    reader.lines().map(|x| x.unwrap()).collect()
+}
+
+fn get_freq(input: &str) -> HashMap<char, i32> {
+    input.chars().fold(
+        HashMap::new(),
+        |mut acc, c| {
+            *acc.entry(c).or_insert(0) += 1;
+            acc
+        })
+}
+
+fn has_exactly_2_or_3(input: &str) -> (bool, bool) {
+    let freq = get_freq(input);
+    (freq.values().any(|&x| x == 2), freq.values().any(|&x| x == 3))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = read_input();
+    let res = input.clone()
+        .iter()
+        .map(|x| has_exactly_2_or_3(&x))
+        .fold((0,0), |acc, x| (acc.0 + if x.0 {1} else {0}, acc.1 + if x.1 {1} else {0}));
+
+    println!("Result {:?}", res.0 * res.1);
+
+    'outer: for x in input.clone() {
+        for y in input.clone() {
+            let count = x.chars().zip(y.chars()).filter(|x| x.0 != x.1).count();
+            //println!("{}:{} - {} diffs", x, y, count);
+            if (count == 1) {
+                println!("{}:{} - diffs\n{}", x, y, x.chars().zip(y.chars()).filter(|x| x.0 == x.1).map(|x| x.0).collect::<String>());
+                break 'outer;
+            }
+        }
+    }
 }
