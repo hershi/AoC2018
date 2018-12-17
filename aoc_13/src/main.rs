4ef141522ffaa8fb03872cc434e79ae8d8c1b07f
Guy Hershenbaum <hershi@fb.com>
2018-12-16 21:41:47 -0800

    Day 13: next-tick initial implementation (so much code :( )

--- aoc_13/src/main.rs
+++ aoc_13/src/main.rs
@@ -1,31 +1,91 @@
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
-use std::collections::HashMap;
 
-#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+use std::collections::HashMap;
+#[derive(Debug, PartialEq, Eq, Hash, Clone, PartialOrd, Ord)]
 struct Point {
     x: usize,
     y: usize,
 }
 
-#[derive(Debug)]
+impl Point {
+    fn next(&self, dir: &Direction) -> Point {
+        match dir {
+            Direction::Up => Point{x: self.x, y: self.y - 1},
+            Direction::Down => Point{x: self.x, y: self.y + 1},
+            Direction::Left => Point{x: self.x - 1, y: self.y},
+            Direction::Right => Point{x: self.x + 1, y: self.y},
+            Direction::Crash => Point{x: self.x, y: self.y},
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 enum Direction {
     Up,
     Down,
     Left,
     Right,
+    Crash,
 }
 
-#[derive(Debug)]
+impl Direction {
+    fn turn_left(&self) -> Direction {
+        match self {
+            Direction::Up => Direction::Left,
+            Direction::Left => Direction::Down,
+            Direction::Down => Direction::Right,
+            Direction::Right => Direction::Up,
+            Direction::Crash => Direction::Crash,
+        }
+    }
+
+    fn turn_right(&self) -> Direction {
+        match self {
+            Direction::Up => Direction::Right,
+            Direction::Right => Direction::Down,
+            Direction::Down => Direction::Left,
+            Direction::Left => Direction::Up,
+            Direction::Crash => Direction::Crash,
+        }
+    }
+
+    fn turn(&self, t: &Turn) -> Direction{
+        match t {
+            Turn::Left => self.turn_left(),
+            Turn::Straight => self.clone(),
+            Turn::Right => self.turn_right(),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+enum Turn {
+    Left,
+    Straight,
+    Right,
+}
+
+impl Turn {
+    fn next(&self) -> Turn {
+        match self {
+            Turn::Left => Turn::Straight,
+            Turn::Straight => Turn::Right,
+            Turn::Right => Turn::Left,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 struct Cart {
     direction: Direction,
-    next_turn: Direction,
+    next_turn: Turn,
 }
 
 impl Cart {
     fn new(direction: Direction) -> Cart {
-        Cart { direction, next_turn: Direction::Left }
+        Cart { direction, next_turn: Turn::Left }
     }
 
     fn to_char(&self) -> char {
@@ -34,6 +94,7 @@ impl Cart {
             Direction::Down => 'v',
             Direction::Left => '<',
             Direction::Right => '>',
+            Direction::Crash => 'X',
         }
     }
 }
@@ -112,12 +173,79 @@ fn print_state(rails: &Rails, carts: &Carts) {
     }
 }
 
+fn next_state(pos: &Point, rail: &Rail, cart: Cart) -> (Point, Cart) {
+    match rail {
+        Rail::Vertical => {
+            match cart.direction {
+                Direction::Up | Direction::Down => (pos.next(&cart.direction), cart),
+                _ => panic!("Direction doesn't match rail: {:?} {:?} {:?}", rail, cart.direction, pos)
+            }
+        },
+        Rail::Horizontal => {
+            match cart.direction {
+                Direction::Left | Direction::Right => (pos.next(&cart.direction), cart),
+                _ => panic!("Direction doesn't match rail: {:?} {:?} {:?}", rail, cart.direction, pos)
+            }
+        },
+        Rail::Curve1 => {
+            // Curve 1: '/'
+            match cart.direction {
+                Direction::Up => (pos.next(&Direction::Right), Cart{direction: Direction::Right, next_turn: cart.next_turn}),
+                Direction::Down => (pos.next(&Direction::Left), Cart{direction: Direction::Left, next_turn: cart.next_turn}),
+                Direction::Left => (pos.next(&Direction::Down), Cart{direction: Direction::Down, next_turn: cart.next_turn}),
+                Direction::Right => (pos.next(&Direction::Up), Cart{direction: Direction::Up, next_turn: cart.next_turn}),
+                _ => panic!("Direction doesn't match rail: {:?} {:?} {:?}", rail, cart.direction, pos)
+            }
+        },
+        Rail::Curve2 => {
+            // Curve 1: '\\'
+            match cart.direction {
+                Direction::Up => (pos.next(&Direction::Left), Cart{direction: Direction::Left, next_turn: cart.next_turn}),
+                Direction::Down => (pos.next(&Direction::Right), Cart{direction: Direction::Right, next_turn: cart.next_turn}),
+                Direction::Left => (pos.next(&Direction::Up), Cart{direction: Direction::Up, next_turn: cart.next_turn}),
+                Direction::Right => (pos.next(&Direction::Down), Cart{direction: Direction::Down, next_turn: cart.next_turn}),
+                _ => panic!("Direction doesn't match rail: {:?} {:?} {:?}", rail, cart.direction, pos)
+            }
+        },
+        Rail::Intersection => {
+            let dir = cart.direction.turn(&cart.next_turn);
+            (pos.next(&dir), Cart{direction: dir, next_turn: cart.next_turn.next()})
+        },
+    }
+}
+
+fn next_tick(rails: &Rails, carts: &mut Carts) {
+    let mut positions = carts.keys().map(|k|k.clone()).collect::<Vec<(Point)>>();
+    positions.sort();
+
+    for p in positions {
+        // This covers two scenarios:
+        // This was a crash even before - then it should remain static
+        // One of the previously moved carts crashes with the cart we're about to move. By
+        // skipping it we're basically merging the two carts at the crash site.
+        if carts.get(&p).map_or(false, |c|c.direction == Direction::Crash) { continue; }
+
+        let (next_pos, next_cart) = next_state(&p, rails.get(&p).unwrap(), carts.remove(&p).unwrap());
+        if carts.contains_key(&next_pos) {
+            carts.insert(next_pos, Cart::new(Direction::Crash));
+        } else {
+            carts.insert(next_pos, next_cart);
+        }
+    }
+}
+
 fn main() {
-    let (rails, carts) = read_input();
+    let (rails, mut carts) = read_input();
     rails.iter().for_each(|c| println!("{:?}", c));
 
     println!("");
     carts.iter().for_each(|c| println!("{:?}", c));
 
     print_state(&rails, &carts);
+
+    for i in 1..4 {
+        println!("\nTick {}", i);
+        next_tick(&rails, &mut carts);
+        print_state(&rails, &carts);
+    }
 }
